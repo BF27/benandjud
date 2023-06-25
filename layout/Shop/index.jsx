56eba1d66cd792e4ce3a79bc153eea4859b79bd0
Farkas Bence <farkas.bence27@gmail.com>
2023-06-25 14:15:41 +0200

    add ProductCard component skeleton

--- layout/Shop/index.jsx
+++ layout/Shop/index.jsx
@@ -1,9 +1,17 @@
+import ProductCard from "../../components/ProductCard";
+import "./styles/shop.css";
+
 const Shop = () => {
   return (
-    <section id="shop" className="shop">
-      <h2>Shop</h2>
+    <section id="shop" className="shop container-fluid position-relative">
+      <div className="container">
+        <h2 className="text-center">Shop</h2>
+        <div className="product-card-container d-flex flex-wrap">
+          <ProductCard />
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
