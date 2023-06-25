49feb98829358bb80bc7b39dffc90abedccb1715
Farkas Bence <farkas.bence27@gmail.com>
2023-06-25 23:42:28 +0200

    create contact section and footer

--- layout/Contact/index.jsx
+++ layout/Contact/index.jsx
@@ -1,9 +1,21 @@
+import "./styles/contact.css";
+
 const Contact = () => {
   return (
-    <section>
-      <h2>Contact</h2>
+    <section id="contact" className="contact container-fluid d-flex flex-column gap-4">
+      <header className="container d-flex justify-content-center align-items-center">
+        <h2>Contact Us</h2>
+      </header>
+      <div className="container d-flex justify-content-center align-items-center">
+        <form className="d-flex flex-column align-items-center justify-content-center gap-3 p-4">
+          <label htmlFor="name"> <input type="text" name="name" id="name" placeholder="Name" /> </label>
+          <label htmlFor="email"> <input type="email" name="email" id="email" placeholder="Email" /> </label>
+          <textarea name="message" id="message" placeholder="Type your message here..."></textarea>
+          <button type="button" className="btn form-submit-btn">Send message</button>
+          </form>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- layout/Footer/index.jsx
+++ layout/Footer/index.jsx
@@ -1,9 +1,18 @@
+import "./styles/footer.css";
+
 const Footer = () => {
   return (
-    <footer id="footer" className="footer">
-      <h2>Footer</h2>
+    <footer id="footer" className="footer container-fluid row">
+      <div className="footer-container container d-flex flex-column-reverse align-items-center flex-md-row  gap-3 gap-md-5 p-3">
+        <img src="/svg/logo-01.svg" alt="logo" className="col" />
+        <p className="col col-md-3">
+          Sweet goodbyes from Ben & Jud's Ice Cream Shop! Thank you for savoring
+          our delectable treats. Until we meet again, may your days be filled
+          with happiness and a scoop of delight. Stay sweet!
+        </p>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
