f329bf69212cf92724ac240f3fd62b475b75f34a
Farkas Bence <farkas.bence27@gmail.com>
2023-06-26 08:46:11 +0200

    edit styles

--- layout/Footer/index.jsx
+++ layout/Footer/index.jsx
@@ -3,13 +3,15 @@ import "./styles/footer.css";
 const Footer = () => {
   return (
     <footer id="footer" className="footer container-fluid row">
-      <div className="footer-container container d-flex flex-column-reverse align-items-center flex-md-row  gap-3 gap-md-5 p-3">
-        <img src="/svg/logo-01.svg" alt="logo" className="col" />
-        <p className="col col-md-3">
-          Sweet goodbyes from Ben & Jud's Ice Cream Shop! Thank you for savoring
-          our delectable treats. Until we meet again, may your days be filled
-          with happiness and a scoop of delight. Stay sweet!
-        </p>
+      <div className="container">
+        <div className="footer-container d-flex flex-column-reverse align-items-center flex-md-row  gap-3 gap-md-5 p-3">
+          <img src="/svg/logo-01.svg" alt="logo" className="col" />
+          <p className="col col-md-3">
+            Sweet goodbyes from Ben & Jud's Ice Cream Shop! Thank you for
+            savoring our delectable treats. Until we meet again, may your days
+            be filled with happiness and a scoop of delight. Stay sweet!
+          </p>
+        </div>
       </div>
     </footer>
   );
