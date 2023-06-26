3edaaaa1ff29a25a0269dc01131c43005897e201
Farkas Bence <farkas.bence27@gmail.com>
2023-06-26 23:56:19 +0200

    add basketItems render the counts

--- components/BasketItem/index.jsx
+++ components/BasketItem/index.jsx
@@ -4,34 +4,31 @@ import "./styles/basketItem.css";
 
 const BasketItem = ({ id, name, price, imgUrl }) => {
   const basket = JSON.parse(localStorage.getItem("basket"));
-  const [count, setCount] = useState(null);
+  const [quantity, setQuantity] = useState(null);
 
   useEffect(() => {
-    let counter = 0;
-    basket.map((item) => {
-      if (item.id === id) {
-        counter++;
-      }
-    });
-    setCount(counter);
+    const basketItem = basket.filter((item) => id === item.product.id);
+    console.log(basketItem);
+    setQuantity(basketItem[0].amount);
   }, []);
 
-  
-    return (
-      <li className="basket-item d-flex align-items-center gap-3 m-3 pb-3">
-        <div className="basket-item-image">
-          <img src={imgUrl} alt="product" />
+  return (
+    <li className="basket-item d-flex align-items-center gap-3 m-3 pb-3">
+      <div className="basket-item-image">
+        <img src={imgUrl} alt="product" />
+      </div>
+      <div className="basket-item-details">
+        <h4 className="basket-item-name">{name}</h4>
+        {quantity && <AmountCounter amount={quantity} />}
+        <div className="d-flex justify-content-between gap-5 pt-3">
+          <span className="basket-item-price">{`${price * quantity}FT`}</span>
+          <span id={`remove_${id}`} className="remove">
+            remove
+          </span>
         </div>
-        <div className="basket-item-details">
-          <h4 className="basket-item-name">{name}</h4>
-          {count && <AmountCounter amount={count} />}
-          <div className="d-flex justify-content-between gap-5 pt-3">
-            <span className="basket-item-price">{`${price * count}FT`}</span>
-            <span className="remove">remove</span>
-          </div>
-        </div>
-      </li>
-    );
+      </div>
+    </li>
+  );
 };
 
 export default BasketItem;
