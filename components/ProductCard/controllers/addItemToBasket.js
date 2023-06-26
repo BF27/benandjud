d628d818fc12250cc6483e91c6bacb71759210cc
Farkas Bence <farkas.bence27@gmail.com>
2023-06-26 13:37:59 +0200

    edit basket dropdown styles

--- components/ProductCard/controllers/addItemToBasket.js
+++ components/ProductCard/controllers/addItemToBasket.js
@@ -1,14 +1,26 @@
 import { db } from "../../../utils/initFirebase";
-import { doc, collection, getDoc } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
 
-const addItemToBasket =  (event) => {
+const addItemToBasket = (event) => {
   const itemId = event.target.id.split("_")[1];
+  const basket = localStorage.getItem("basket");
 
-  const getItemFromDb = async () => {
-    const itemRef =  collection(db,"products");
-    return item.data();
-  }
-  console.log(getItemFromDb())
+  const itemRef = doc(db, "products", itemId);
+  getDoc(itemRef).then((docSnap) => {
+    if (docSnap) {
+      const item = docSnap.data();
+      const basketItem = {
+        id: item.id,
+        name: item.title,
+        price: item.price,
+        imgUrl: item.imgUrl,
+      };
+      basket.push(basketItem);
+      console.log(basket);
+    } else {
+      console.log("No such document!");
+    }
+  });
 };
 
 export default addItemToBasket;
