1dbc1259d1f63235387a5f8ad504f5bfa668a4d6
Farkas Bence <farkas.bence27@gmail.com>
2023-06-27 08:40:50 +0200

    change order of + - buttons

--- components/AmountCounter/index.jsx
+++ components/AmountCounter/index.jsx
@@ -1,17 +1,15 @@
-import { useEffect, useState } from "react";
-
 import "./styles/amountCounter.css";
 
 const AmountCounter = ({ amount }) => {
 
   return (
     <div className="amount-counter d-flex justify-content-between">
-      <button className="plus btn d-flex align-items-center justify-content-center">
-        +
-      </button>
-      <span className="amount">{amount}</span>
       <button className="minus btn d-flex align-items-center justify-content-center">
         -
+      </button>
+      <span className="amount">{amount}</span>
+        <button className="plus btn d-flex align-items-center justify-content-center">
+        +
       </button>
     </div>
   );
