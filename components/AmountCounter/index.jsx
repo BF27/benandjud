940b3ae0aebea907c4a1706b8643c75c1abf47fa
Farkas Bence <farkas.bence27@gmail.com>
2023-06-28 15:49:43 +0200

    add handleMinus and handlePlus function

--- components/AmountCounter/index.jsx
+++ components/AmountCounter/index.jsx
@@ -1,18 +1,46 @@
 import "./styles/amountCounter.css";
+import { BasketContext } from "../../contexts/BasketContext";
+import { UpdateBasketContext } from "../../contexts/UpdateBasketContext";
+import { useContext } from "react";
 
 const AmountCounter = ({ quantity, id }) => {
+  const basket = useContext(BasketContext);
+  const updateBasket = useContext(UpdateBasketContext);
+
+  const handlePlus = () => {
+    const newBasket = basket.map((item) => {
+      if (item.product.id === id) {
+        item.amount = quantity + 1;
+      }
+      return item;
+    });
+    localStorage.setItem("basket", JSON.stringify(newBasket));
+    updateBasket();
+  };
+
+  const handleMinus = () => {
+    const newBasket = basket.map((item) => {
+      if (item.product.id === id) {
+        item.amount = quantity - 1;
+      }
+      return item;
+    });
+    localStorage.setItem("basket", JSON.stringify(newBasket));
+    updateBasket();
+  }
+
   return (
     <div className="amount-counter d-flex justify-content-between">
       <button
         className="minus btn d-flex align-items-center justify-content-center"
-        /* onClick={() => handleMinus()} */
+        onClick={() => handleMinus()}
       >
         -
       </button>
       {<span className="amount">{quantity}</span>}
       <button
         className="plus btn d-flex align-items-center justify-content-center"
-        /* onClick={() => handlePlus()} */
+        onClick={() => handlePlus()}
       >
         +
       </button>
--- components/ProductCard/index.jsx
+++ components/ProductCard/index.jsx
@@ -25,7 +25,7 @@ const ProductCard = ({ product }) => {
       }
     }
 
-  }, []);
+  }, [basket]);
 
   useEffect(() => {
     updateBasket();
